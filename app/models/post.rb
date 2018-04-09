3e9e8bfcf495819b696ee2e273780dc84993b188
Jonathan Pepin <jypepin@gmail.com>
2018-04-09 18:30:35 +0200

    Post model with tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  after_initialize :set_slug
+
+  def set_slug
+    self.slug = self.title ? self.title.parameterize : ""
+  end
+
+  def self.get_all
+    Post.order(:created_at => :desc)
+  end
 end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe Post, :type  => :model do
+  context "at creation" do
+    it "saves with a title and a content" do
+      post = Post.new(:title => "this is title", :content => "this is content")
+      expect(post.title).to eq "this is title"
+      expect(post.content).to eq "this is content"
+    end
+
+    it "generates a slug" do
+      Post.create(:title => "this is a title")
+      expect(Post.last.slug).to eq "this-is-a-title"
+    end
+
+    it "generates an empty slug when title is empty" do
+      Post.create
+      expect(Post.last.slug).to eq ""
+    end
+  end
+
+  context "loading posts" do
+    it "returns posts sorted by creation date descending" do
+      post1 = Post.create
+      post2 = Post.create
+      
+      all_posts = Post.get_all
+
+      expect(all_posts[0].created_at).to be > all_posts[1].created_at
+    end
+  end
+end
