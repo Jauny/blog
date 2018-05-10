93e4160002ce4dfacf166421d132fbf34734d1cd
Jonathan Pepin <jypepin@gmail.com>
2018-05-10 18:16:16 +0200

    make former jekyll url work

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, :path => "/", :param => :slug
 
+  # I want to keep former url format from Jekyll to work.
+  # Format is /year/slug.
+  get "/:year/:slug/", :to => "posts#show", :constraints => { :year => /\d{4}/ }
+
   root :to => "posts#index"
 end
