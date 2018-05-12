0898a051ecbfddc36974688622715cac2bba65c2
Jonathan Pepin <jypepin@gmail.com>
2018-05-12 10:20:21 +0200

    add lolz

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def lolz
+    render "lolz/index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  root :to => "posts#index"
+
+  # Active Admin
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  get "/lolz", :to => "application#lolz"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, :path => "/", :param => :slug
 
   # I want to keep former url format from Jekyll to work.
   # Format is /year/slug.
   get "/:year/:slug/", :to => "posts#show", :constraints => { :year => /\d{4}/ }
-
-  root :to => "posts#index"
 end
